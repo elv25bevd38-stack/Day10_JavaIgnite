65e225ea1d5af20c7299842ceee056bab6f18f42
Ayush Samanta <elv.25bevd38@silicon.ac.in>
2026-06-06 01:14:59 +0530

    Implement safe index retrieval for string characters
    Added input handling for string index retrieval.

--- String_Processor.java
+++ String_Processor.java
@@ -17,3 +17,36 @@ StringIndexOutOfBoundsException
 Example:
 Invalid index handled safely
 */
+import java.util.Scanner;
+public class String_Processor 
+{
+    
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String str ;
+        System.out.print("Enter A String : ");
+        str = sc.nextLine();
+        int index;
+        System.out.print("Enter The Index You Want To Get Its Character :");
+        index = sc.nextInt();
+        try
+        {
+            if( index > str.length())
+            {
+                throw new StringIndexOutOfBoundsException("Invalid index !!!");
+            }
+            else
+            {
+                System.out.println("Character at "+index+" is "+str.charAt(index));
+                System.out.println(str.charAt(index));
+
+            }
+
+        }
+        catch(StringIndexOutOfBoundsException e)
+        {
+            System.out.println("Invalid index !!!");
+        }
+    }
+}
