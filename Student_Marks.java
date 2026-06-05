64da6eddb5b084644a9df46075a3b749d7a77f34
Ayush Samanta <elv.25bevd38@silicon.ac.in>
2026-06-06 00:54:11 +0530

    Implement exception handling for student marks input
    Added exception handling for invalid index and input types.

--- Student_Marks.java
+++ Student_Marks.java
@@ -10,3 +10,43 @@ Add Exception Handling:
 Handle invalid index
 Handle invalid input (if user enters text instead of number)
 */
+import java.util.Scanner;
+import java.util.InputMismatchException;
+public class Student_Marks 
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String marks[] = new String[5];
+        System.out.println("Enter the marks of 5 Students ");
+        for(int i=0 ; i<5 ; i++)
+        {
+            System.out.print("Enter The Mark of "+(i+1)+" Student :");
+            marks[i] = sc.nextLine();
+
+        }
+        System.out.print("Enter The Index Of Student to Display Marks : ");
+        int index = sc.nextInt();
+        try
+        {
+            if(index > 5)
+            {
+                throw new IndexOutOfBoundsException("Invalid Input !!!");
+
+            }
+            
+            
+            else
+            {
+                
+                System.out.println("Mark of "+index+" Student is "+marks[index-1]);
+            }
+        } 
+        catch(IndexOutOfBoundsException e)
+        {
+            System.out.println("Invalid Input !!!");
+        }
+
+    }
+    
+}
