ce61f26766e1d1b2d4c9c111e0d0f328cd4b2cae
Ayush Samanta <elv.25bevd38@silicon.ac.in>
2026-06-06 01:13:17 +0530

    Implement multi-level exception handling in ATM
    Added exception handling for withdrawal transactions.

--- ATM_check.java
+++ ATM_check.java
@@ -11,3 +11,43 @@ If input is invalid (text instead of number) → handle exception
 Expected Output:
 Transaction Successful / Insufficient Balance / Invalid Input
 */
+import java.util.Scanner;
+import java.util.InputMismatchException;
+public class ATM_Multi_Level_Exception
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        double balance , withdrawl;
+        try
+        {
+            System.out.print("Enter Balance : ");
+            balance = sc.nextDouble();
+            System.out.print("Enter withdrawl Amount : ");
+            withdrawl = sc.nextDouble();
+            if(withdrawl > balance)
+            {
+                throw new Exception("Insufficient Balance");
+            }
+            else if(withdrawl < 0)
+            {
+                throw new InputMismatchException("Invalid Input");
+            }
+            else
+            {
+                System.out.println("Transaction Successful ");
+                System.out.println("Current Balance : "+(balance-withdrawl));
+            }
+
+
+        }
+        catch(InputMismatchException e)
+        {
+            System.out.println("Invalid Input");
+        }
+        catch(Exception e)
+        {
+            System.out.println("Insufficient Balance");
+        }
+    }
+}
