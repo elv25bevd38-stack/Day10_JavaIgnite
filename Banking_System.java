7e1afb2d757fbe30609a66c724725805fc81efe6
Ayush Samanta <elv.25bevd38@silicon.ac.in>
2026-06-06 00:52:48 +0530

    Implement withdrawal with balance check and exception
    Added exception handling for withdrawal process.

--- Banking_System.java
+++ Banking_System.java
@@ -11,3 +11,35 @@ If invalid input → handle exception
 Output:
 Withdrawal successful OR Insufficient balance
 */
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Banking_System 
+{
+    public static void main(String[] args )
+{ 
+    Scanner sc = new Scanner(System.in);
+    double balance,withdrawl;
+
+    try
+    {
+            System.out.print("Enter Balance : ");
+            balance = sc.nextDouble();
+            System.out.print("Enter withdrawl Amount : ");
+            withdrawl = sc.nextDouble();
+            if(withdrawl > balance)
+            {
+                throw new Exception("Insufficient balance");
+            }
+            else
+            {
+                System.out.println("Withdrawal successful");
+                System.out.println("Current Balance : "+(balance-withdrawl));
+            }
+    }
+    catch(Exception e)
+    {
+        System.out.println("Insufficient balance");
+    }
+}
+}
